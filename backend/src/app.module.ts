2f8fc29a01f6eca8a5db802953719e5507ceaceb
AndreiaDomingos <andreia.adserafim@gmail.com>
2025-04-27 20:43:46 -0300

    feat: enhance ConfigModule initialization to be globally accessible

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { LembreteModule } from './lembrete/lembrete.module';
 import { MailService } from './mail/mail.service';
-import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), LembreteModule],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+  }), LembreteModule],
   providers: [PrismaService, MailService],
 })
 export class AppModule {}
