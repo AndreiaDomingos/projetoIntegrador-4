a145bfa1544af47b5bd5210e1ec1e9a40ebb9198
AndreiaDomingos <andreia.adserafim@gmail.com>
2025-04-27 20:33:17 -0300

    feat: add MailService to LembreteModule providers

--- backend/src/lembrete/lembrete.module.ts
+++ backend/src/lembrete/lembrete.module.ts
@@ -1,13 +1,11 @@
-// backend/src/lembrete/lembrete.module.ts
-
 import { Module } from '@nestjs/common';
 import { LembreteService } from './lembrete.service';
 import { LembreteController } from './lembrete.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { MailService } from '../mail/mail.service'; // <-- IMPORTAR o MailService
 
 @Module({
   controllers: [LembreteController],
-  providers: [LembreteService, PrismaService],
+  providers: [LembreteService, PrismaService, MailService], // <-- ADD aqui também
 })
 export class LembreteModule {}
-
